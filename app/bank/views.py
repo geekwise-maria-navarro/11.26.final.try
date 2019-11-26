bfdca916350858ef68f4fd74d5a7d1cb728003e7
geekwise-maria-navarro <geekwise.maria.navarro@gmail.com>
2019-11-26 23:23:39 +0000

    added serializer viewsets to viewspy

--- app/bank/views.py
+++ app/bank/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from bank.serializers import User_Serializer, Group_Serializer
 
-# Create your views here.
+class User_Viewsets(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User_Serializer
+    serializer_class = User_Serializer
+
+class Group_Viewset(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = Group_Serializer
